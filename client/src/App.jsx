ac7b58d8d2b01bcf8bfa34357afdfe3a86764362
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-06-17 11:33:16 +0200

    ça avance

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
+import { useLoaderData } from "react-router-dom";
 import "./App.css";
 
 function App() {
+  const items = useLoaderData();
+  console.log(items);
   return <h1> Sommething will bien here </h1>;
 }
 
