3dd692eec10a2612ed01ff72fba527347bb70e47
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-06-17 15:23:30 +0200

    voilà voilà

--- server/app/router.js
+++ server/app/router.js
@@ -12,25 +12,7 @@ const itemActions = require("./controllers/itemActions");
 // Route to get a list of items
 router.get("/items", itemActions.browse);
 // Route to get a specific item by ID
-router.get("/items/:id", (req, res) => {
-  const itemId = req.params.id;
-  client
-    .query(
-      `SELECT p.product_name AS title,
-      p.link_to AS link,
-      c.label AS category, 
-      s.label AS status 
-      FROM product AS p
-      INNER JOIN category AS c ON p.category_id = c.id 
-      INNER JOIN status AS s ON p.status_id = s.id
-      WHERE p.id = ?`,
-      [itemId]
-    )
-    .then(([product]) => {
-      res.status(200).json(product[0]);
-    });
-});
-
+router.get("/items/:id", itemActions.read);
 // Route to add a new item
 router.post("/items", itemActions.add);
 
