2cf22a93beda5a86c02f35deb0bf2e226202eaf5
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-06-17 16:37:59 +0200

    et du css

--- client/src/pages/Item.jsx
+++ client/src/pages/Item.jsx
@@ -1,14 +1,20 @@
 import { useLoaderData } from "react-router-dom";
 
+import "./Item.css";
+
 function Item() {
   const item = useLoaderData();
   return (
-    <>
-      <h1>{item.title}</h1>
-      <p>{item.category}</p>
-      <p>{item.status}</p>
-      <p>{item.link}</p>
-    </>
+    <div className="item-container">
+      <h1 className="item-title">{item.title}</h1>
+      <p className="item-category">{item.category}</p>
+      <p className="item-status">{item.status}</p>
+      {item.link && (
+        <p className="item-link">
+          <a href={item.link}>Link</a>
+        </p>
+      )}
+    </div>
   );
 }
 
