6183ae1ee61c5766eda55baaf73a4b301881c4e7
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-06-17 14:13:29 +0200

    create route

--- server/app/router.js
+++ server/app/router.js
@@ -29,7 +29,24 @@ router.get("/items", (req, res) => {
 });
 
 // Route to get a specific item by ID
-router.get("/items/:id", itemActions.read);
+router.get("/items/:id", (req, res) => {
+  const itemId = req.params.id;
+  client
+    .query(
+      `SELECT p.product_name AS title,
+      p.link_to AS link,
+      c.label AS category, 
+      s.label AS status 
+      FROM product AS p
+      INNER JOIN category AS c ON p.category_id = c.id 
+      INNER JOIN status AS s ON p.status_id = s.id
+      WHERE p.id = ?`,
+      [itemId]
+    )
+    .then(([product]) => {
+      res.status(200).json(product[0]);
+    });
+});
 
 // Route to add a new item
 router.post("/items", itemActions.add);
